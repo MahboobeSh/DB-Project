17a901a42304600dea05b4d0cbc1ac8cf4efdfa4
MahboobeSh <mahboobe.shakeri98@gmail.com>
2021-07-20 14:22:46 -0400

    setting changed to contain postgresql

--- dbproject/settings.py
+++ dbproject/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'dbproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'projectdb',
+        'USER': 'mahboobe',
+        'PASSWORD': '9531301',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
