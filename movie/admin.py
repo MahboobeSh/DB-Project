d9374dac0b638a65c6396deec9599ab097cfac20
MahboobeSh <mahboobe.shakeri98@gmail.com>
2021-07-20 13:28:01 -0400

    some models added

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import User, ProUser, Movie, SepecialMovie, Watch, Opinion, SepecialMovieWatch, Tag
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(ProUser)
+admin.site.register(Movie)
+admin.site.register(SepecialMovie)
+admin.site.register(Watch)
+admin.site.register(Opinion)
+admin.site.register(SepecialMovieWatch)
+admin.site.register(Tag)
--- movie/models.py
+++ movie/models.py
@@ -13,7 +13,7 @@ class User(models.Model):
     nationalID = models.CharField(max_length=10)
     wallet = models.IntegerField(default=0)
     points = models.IntegerField(default=0)
-    introducer = models.ForeignKey('self',null=True, on_delete=models.SET_NULL)
+    introducer = models.ForeignKey('self',null=True, blank=True,on_delete=models.SET_NULL)
 
 
 class ProUser(models.Model):
@@ -21,7 +21,12 @@ class ProUser(models.Model):
     expr_date = models.DateField()
 
 
-class Movie(models.model):
+class Tag(models.Model):
+    tag_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, unique=True)
+
+
+class Movie(models.Model):
     movie_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=256)
     year = models.IntegerField()
@@ -29,20 +34,43 @@ class Movie(models.model):
     producer = models.CharField(max_length=1024)
     file = models.CharField(max_length=256)
 
-class SepecialMovie(models.model):
+class SepecialMovie(models.Model):
     special_movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE, primary_key=True)
     price = models.IntegerField()
 
 
-class Watch(models.model):
+class Watch(models.Model):
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     watch_time = models.DateTimeField(auto_now_add=True)
 
 
-class Opinion(models.model):
+
+class SepecialMovieWatch(models.Model):
+    movie_id = models.ForeignKey(SepecialMovie, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    watch_time = models.DateTimeField(auto_now_add=True)
+
+
+class Opinion(models.Model):
     rate = models.IntegerField()
     comment = models.CharField(max_length=256, null=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+class MovieTag(models.Model):
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+
+
+class List(models.Model):
+    list_id = models.AutoField(primary_key=True)
+    prouser_id = models.ForeignKey(ProUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=1024)
+
+class MovieList(models.Model):
+    list_id = models.AutoField(primary_key=True)
+    models.ForeignKey(Movie, on_delete=models.CASCADE)
 
